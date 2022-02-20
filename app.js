30834b116eb02e298053cfd2c7043baffaa54f89
Sarbjot Singh <sarbjot_14@hotmail.com>
2022-02-20 14:11:40 -0800

    adding

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+
+// Create express app
+const app = express();
+
+console.log(process.env.DATABASE_PASSWORD)
+// Database
+mongoose.connect(
+  'mongodb+srv://SarbjotSingh:' +
+  process.env.DATABASE_PASSWORD +
+    '@cluster0.w61qd.mongodb.net/' +process.env.DATABASE_NAME+'?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to MongoDB database...');
+});
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const QuotesRoute = require('./routes/Quotes');
+
+app.use('/quotes', QuotesRoute);
+
+// Starting server
+app.listen(3000, console.log('Listening on port 3000'));
